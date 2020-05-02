ff5d6dcd7e08c47e45af46424562b69b0760a21f
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-05-02 13:51:17 +0800

    Make ports dynamic for heroku deployment

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post("/api/email/send", (req, res) => {
     });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
